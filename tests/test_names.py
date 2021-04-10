e7cd315163539b63be9c8cba2b7b21da9516c938
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-04-09 22:41:36 -0500

    Update test_names.py

--- tests/test_names.py
+++ tests/test_names.py
@@ -12,4 +12,4 @@ def test_names():
     data, list_names, count = redactor.redact_names (s)
     
     # there are 3 names in this file [Lily Potter , Harry Potter , James Potter]
-    assert len(list_names) == 7 
+    assert len(list_names) == 3
