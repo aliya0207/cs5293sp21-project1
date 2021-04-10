77c23c13f2321482b36936a485fbf7e3a773ab50
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-04-09 22:47:03 -0500

    Create test_dates.py

---
+++ tests/test_dates.py
@@ -0,0 +1,16 @@
+import project1
+import spacy
+from project1 import redactor
+import nltk
+import pytest
+
+
+
+
+d = """ Born and raised in central Argentina, Messi relocated to Spain to join Barcelona at age 13, for whom he made his competitive debut aged 17 in October 2004. He established himself as an integral player for the club within the next three years, and in his first uninterrupted season in 2008"""
+
+
+def test_date():
+    (data, list_dates, count) = redactor.redact_dates(d)
+    assert c ==2
+    assert ['October 2004' , '2008'] == list_dates
