3d524bc9f118447af28a8deb7d5ebc609a00bf9e
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-04-09 22:51:53 -0500

    Update test_names.py

--- tests/test_names.py
+++ tests/test_names.py
@@ -9,7 +9,7 @@ s = " Lily Potter died saving her son Harry Potter which ended up creating a shi
 
 
 def test_names():
-    data, list_names, count = redactor.redact_names (s)
+    data, list_names, count = redactor.sanitize_names(s)
     
     # there are 3 names in this file [Lily Potter , Harry Potter , James Potter]
     assert len(list_names) == 3
