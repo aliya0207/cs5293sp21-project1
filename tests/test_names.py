86ccc90abb7c34c22ca880352774dd0d1351ec8a
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-04-09 22:41:24 -0500

    Create test_names.py

---
+++ tests/test_names.py
@@ -0,0 +1,15 @@
+
+import pytest
+import nltk
+import spacy
+import project1
+from project1 import redactor
+
+s = " Lily Potter died saving her son Harry Potter which ended up creating a shield and helped him survive against the villain. However, Tom Riddle still managed to survive but was very weak."
+
+
+def test_names():
+    data, list_names, count = redactor.redact_names (s)
+    
+    # there are 3 names in this file [Lily Potter , Harry Potter , James Potter]
+    assert len(list_names) == 7 
