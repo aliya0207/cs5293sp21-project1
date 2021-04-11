42d919da9460e64164f4a89bdbf99a314f6426b5
aliya0207 <78556073+aliya0207@users.noreply.github.com>
2021-04-11 09:22:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
 	name='project1',
 	version='1.0',
-	author='You Name',
-	authour_email='your ou email',
+	author='Aliya Shaikh',
+	authour_email='aliyashaikh02@ou.edu',
 	packages=find_packages(exclude=('tests', 'docs')),
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest']	
